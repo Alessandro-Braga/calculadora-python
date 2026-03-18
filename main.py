69301ad0625f470d37646701adbc723248eff224
Alessandro-Braga <alessandrobraga067@gmail.com>
2026-03-18 17:16:33 -0300

    Primeiro código de boas vindas

---
+++ main.py
@@ -0,0 +1,3 @@
+nome = input("Olá! Digite seu nome: ")
+print(f"Olá {nome}! Tudo bem? Essa é uma calculadora que estou desenvolvendo!")
+print("A calculadora pode estar bem básica por agora, mas futuramente ela será capaz de resolver grandes operações!")
