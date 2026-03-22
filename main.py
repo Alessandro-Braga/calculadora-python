ea4a2874056f831a30cdb6dee7a16a383133fd40
Alessandro-Braga <alessandrobraga067@gmail.com>
2026-03-22 16:10:14 -0300

    perguntas de qual conta fazer adicionadas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 nome = input("Olá! Digite seu nome: ")
 print(f"Olá {nome}! Tudo bem? Essa é uma calculadora que estou desenvolvendo!")
 print("A calculadora pode estar bem básica por agora, mas futuramente ela será capaz de resolver grandes operações!")
+print(f"Ok {nome}, qual operação você quer fazer?")
+input("Digite 1 para adição \nDigite 2 para subtração \nDigite 3 para multiplicação \nDigite 4 para divisão \nDigite aqui: ")
