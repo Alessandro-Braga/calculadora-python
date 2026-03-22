6438f3b85601bebfea3d497670fbdf3c2bcd3a37
Alessandro-Braga <alessandrobraga067@gmail.com>
2026-03-22 16:55:40 -0300

    Feature: Adicionando operações básicas.

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ nome = input("Olá! Digite seu nome: ")
 print(f"Olá {nome}! Tudo bem? Essa é uma calculadora que estou desenvolvendo!")
 print("A calculadora pode estar bem básica por agora, mas futuramente ela será capaz de resolver grandes operações!")
 print(f"Ok {nome}, qual operação você quer fazer?")
+conta = int(input("Digite 1 para adição \nDigite 2 para subtração \nDigite 3 para multiplicação \nDigite 4 para divisão \nDigite aqui: "))
+print("Ok, agora, me diga os números")
+num1 = int(input("Digite seu número aqui: "))
+num2 = int(input("Digite seu número aqui: "))
 
-conta = input("Digite 1 para adição \nDigite 2 para subtração \nDigite 3 para multiplicação \nDigite 4 para divisão \nDigite aqui: ")
-if conta == "1":
-    num1 = Int(input("Digite seu número aqui: "))
-    num2 = Int(input("Digite seu número aqui: "))
-    print(num1 + num2)
+if conta == 1:
+    print(f"Seu resultado: {num1 + num2}")
+
+elif conta == 2:
+    print(f"Seu resultado: {num1 - num2}")
+
+elif conta == 3:
+    print(f"Seu resultado: {num1 * num2}")
+
+else:
+    print("Você quer fazer uma divisão exata (sem casas decimais) ou uma divisão não exata (com casas decimais)?")
+    div = int(input("Digite 1 para divisão exata \nDigite 2 para divisão não exata"))
+    if div == 1:
+        print(f"Seu resultado: {num1 // num2}")
+    else:
+        print(f"Seu resultado: {num1 / num2}")
