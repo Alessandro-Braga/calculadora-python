4ff72431a7cefa351ec876a38085d75440cf3898
Alessandro-Braga <alessandrobraga067@gmail.com>
2026-03-22 16:50:51 -0300

    Feature: adicionando as  operações.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ nome = input("Olá! Digite seu nome: ")
 print(f"Olá {nome}! Tudo bem? Essa é uma calculadora que estou desenvolvendo!")
 print("A calculadora pode estar bem básica por agora, mas futuramente ela será capaz de resolver grandes operações!")
 print(f"Ok {nome}, qual operação você quer fazer?")
-conta = input("Digite 1 para adição \nDigite 2 para subtração \nDigite 3 para multiplicação \nDigite 4 para divisão \nDigite aqui: ")
 
-if conta == 1:
+conta = input("Digite 1 para adição \nDigite 2 para subtração \nDigite 3 para multiplicação \nDigite 4 para divisão \nDigite aqui: ")
+if conta == "1":
     num1 = Int(input("Digite seu número aqui: "))
     num2 = Int(input("Digite seu número aqui: "))
     print(num1 + num2)
